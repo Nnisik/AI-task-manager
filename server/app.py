e27c0a99b1e8b1f65aec642553f7210b18f27d52
Nina Samykova <nisik0120@gmail.com>
2024-11-21 22:32:41 +0100

    fixed local work as api + other APIs

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 
 # connection to database
 # TODO: transition SQLite database to PostgreSQL
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
@@ -20,24 +22,55 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 # main page
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    ## variant of page activity when were made any changes to database filling
+    ## made using POST http protocol
+#    if request.method == 'POST':
+        # FIXME: add form validation
+#        task_content = request.form['content']
+ #       new_task = Todo(content = task_content)
+  #      try:
+   #         db.session.add(new_task)
+    #        db.session.commit()
+     #       return redirect('/')
+      #  except:
+       #     return "There was an issue adding your task"
+    # else:
+    #    tasks = Todo.query.order_by(Todo.data_created).all()
     return render_template("index.html")
 
-@app.route('/api/get-tasks', methods=['GET'])
-def get_tasks():
+# TODO: API for getting all tasks from database
+@app.route('/api/get_tasks')
+def get_data():
     tasks = Todo.query.order_by(Todo.data_created).all()
-    tasks_set, i = {}, 0
+    tasks_set = []
     for task in tasks:
-        task = {
-            'content': task.content,
-            'completed': task.completed,
-            'data_created': task.data_created
+        task_set = {
+            "id": task.id,
+            "content": task.content,
+            "completed": task.completed,
+            "date": task.data_created
         }
-        tasks_set[i] = task
-        i += 1
-
+        tasks_set.append(task_set)
     return tasks_set
+
+# TODO: APIs
+# TODO: API for creating new task and adding its data into a database
+@app.route('api/create_task', method=['POST'])
+def create_task():
+    pass
+
+# TODO: API for deleting task from database
+@app.route('api/delete_task/<int:id>', method=['POST'])
+def delete_task(id):
+    pass
+
+# TODO: API for updating task
+@app.route('api/update_task/<int:id>', method=['POST'])
+def update_task(id):
+    pass
+
 
 # delete tasks function; then reloads the index page
 @app.route('/delete/<int:id>')
@@ -67,6 +100,7 @@ def update(id):
             return "There was an issue updating your task"
     else:
         return render_template('update.html', task=task)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
