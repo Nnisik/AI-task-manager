107558544c584ba82a59171fde7c007126cf594a
Nina Samykova <nisik0120@gmail.com>
2024-11-21 22:30:52 +0100

    getting tasks api + update task form & sort btn

--- client/index.js
+++ client/index.js
@@ -1,10 +1,72 @@
-// TODO: api connection and drawing all currently stored in database tasks
-// FIXME: returns error in console; in server shows BAD-REQUEST
+// TODO: sorting by name function
+// TODO: sorting by date function
+// TODO: clears "tasks-list" element before refilling
+
+
+// draws a message of task's list is currently empty
+// FIXME: writes "</br>"
+function drawEmptyListMessage() {
+    const emptyListMessage = document.createElement("p");
+    emptyListMessage.classList.add("no-tasks-message");
+    emptyListMessage.innerText = "Looks like there is no tasks you need to do. <br>Time to make plans for future.";
+    document.querySelector("#tasks-list").appendChild(emptyListMessage);
+}
+
 // TODO: separate different object elements creation into different folders
+function drawTaskList(taskList) {
+    // If the gallery element exists, append the posts
+    if (document.querySelector("#tasks-list")) {
+        // Iterate over and append the corresponding post element to the fragment
+        taskList.forEach((task) => {
+            // task container
+            const taskContainer = document.createElement("div");
+            taskContainer.classList.add("task");
+
+            // task info section
+            const taskInfo = document.createElement("div");
+            taskInfo.classList.add("task__info");
+            // task content
+            const taskHeader = document.createElement("h3");
+            taskHeader.innerText = task["content"];
+            taskInfo.appendChild(taskHeader);
+            // task's creation date
+            // FIXME: rework returning date value from full date to "day/month"
+            const taskCreationDate = document.createElement("p");
+            taskCreationDate.innerText = task["date"];
+            taskInfo.appendChild(taskCreationDate);
+            taskContainer.append(taskInfo);
+
+            // task buttons sections
+            const taskOptions = document.createElement("div");
+            taskOptions.classList.add("task__options");
+            // task edit button
+            const editButton = document.createElement("a");
+            editButton.classList.add("task__button");editButton.href = `http://localhost:5000/api/update/${task["id"]}`;
+            const editBtnImg = document.createElement("img");
+            editBtnImg.src = "./src/icons/edit-tools.png";
+            editButton.appendChild(editBtnImg);
+            //delete button
+            const deleteButton = document.createElement("a");
+            deleteButton.classList.add("task__button");
+            deleteButton.href = `http://localhost:5000/api/delete/${task["id"]}`;
+            const deleteBtnImg = document.createElement("img");
+            deleteBtnImg.src = "./src/icons/loschen.png";
+            deleteButton.appendChild(deleteBtnImg);
+
+            taskOptions.append(editButton);
+            taskOptions.append(deleteButton);
+
+            taskContainer.append(taskOptions);
+            document.querySelector("#tasks-list").append(taskContainer);
+        });
+    }
+}
+
+// API connection and drawing all currently stored in database tasks
 function getTasks() {
     try {
         // Fetch the post data from the JSON file
-        fetch("https://localhost:5000/api/get-tasks")
+        fetch("http://127.0.0.1:5000/api/get_tasks")
             .then((response) => {
                 // Check if the response is successful (status 200)
                 if (!response.ok) {
@@ -14,52 +76,7 @@ function getTasks() {
                 return response.json();
             })
             .then((json) => {
-                // If the gallery element exists, append the posts
-                if (document.querySelector("#tasks-list")) {
-                    // Iterate over and append the corresponding post element to the fragment
-                    json.forEach((task) => {
-                        // task container
-                        const taskContainer = document.createElement("div");
-                        taskContainer.classList.add("task");
-
-                        // task info section
-                        const taskInfo = document.createElement("div");
-                        taskInfo.classList.add("task__info");
-                        // task content
-                        const taskHeader = document.createElement("h3");
-                        taskHeader.innerText = task["content"];
-                        taskInfo.appendChild(taskHeader);
-                        // task's creation date
-                        const taskCreationDate = document.createElement("p");
-                        taskCreationDate.innerText = task["data_created"];
-                        taskInfo.appendChild(taskCreationDate);
-                        taskContainer.append(taskInfo);
-
-                        // task buttons sections
-                        const taskOptions = document.createElement("div");
-                        taskOptions.classList.add("task__options");
-                        // task edit button
-                        const editButton = document.createElement("a");
-                        editButton.classList.add("task__button");
-                        editButton.href = `http://localhost:5000/api/update/${task["id"]}`;
-                        const editBtnImg = document.createElement("img");
-                        editBtnImg.src = "./src/icons/edit-tools.png";
-                        editButton.appendChild(editBtnImg);
-                        //delete button
-                        const deleteButton = document.createElement("a");
-                        deleteButton.classList.add("task__button");
-                        deleteButton.href = `http://localhost:5000/api/delete/${task["id"]}`;
-                        const deleteBtnImg = document.createElement("img");
-                        deleteBtnImg.src = "./src/icons/loschen.png";
-                        deleteButton.appendChild(deleteBtnImg);
-                        // task delete button
-                        taskOptions.append(editButton);
-                        taskOptions.append(deleteButton);
-                        taskContainer.append(taskOptions);
-
-                        document.querySelector("#tasks-list").append(taskContainer);
-                    });
-                }
+                return json;
             })
 
     } catch (error) {
@@ -69,6 +86,12 @@ function getTasks() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    getTasks();
-    console.log("Building something that is not sucks ✨");
+    let  taskList = getTasks();
+    if (!taskList) {
+        drawEmptyListMessage();
+    }
+    else {
+        drawTaskList();
+    }
+    console.log("Building something that's not sucks ✨");
 });
