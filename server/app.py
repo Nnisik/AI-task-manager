d8d8adb05ad59afa20e4528c335e940a0cb31e9d
Nnisik <nisik0120@gmail.com>
2024-11-24 23:47:59 +0100

    get and post requests for api

--- server/app.py
+++ server/app.py
@@ -1,81 +1,73 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 # connection to database
 # TODO: transition SQLite database to PostgreSQL
 app = Flask(__name__)
+# allow check api work on localhost
 CORS(app)
+# creating a database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+# api configuration
+api = Api(app)
 
 # new Task class
 # TODO: add more fields
-class Todo(db.Model):
+class TaskModel(db.Model):
+    __tablename__ = 'task_model'
+
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
     data_created  = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return f"Task(content={self.content}, data={self.data_created})"
+
+# tasks arguments
+tasks_args = reqparse.RequestParser()
+tasks_args.add_argument("content", type=str, required=True)
+tasks_args.add_argument("date", type=str, required=True)
+
+#api fields
+taskFields = {
+    'id':fields.Integer,
+    'content':fields.String,
+    'date':fields.DateTime
+}
+
+#tasks API
+class Tasks(Resource):
+    @marshal_with(taskFields)
+    def get(self):
+        tasks = TaskModel.query.all()
+        return tasks
+
+    @marshal_with(taskFields)
+    def post(self):
+        args = tasks_args.parse_args()
+        date_value = datetime.strptime(args["date"], "%Y-%m-%d %H:%M:%S")
+        # FIXME: rename everywhere to "date"
+        # FIXME: date value sets as "null"
+        task = TaskModel(content = args["content"], data_created=date_value)
+        db.session.add(task)
+        db.session.commit()
+        tasks = TaskModel.query.all()
+        return tasks, 201
+
+    # TODO: update option
+    # TODO: delete option
+
+#setting up API resource
+api.add_resource(Tasks, '/api/tasks/')
 
 # main page
 @app.route('/')
 def index():
     return render_template("index.html")
-
-# TODO: API for getting all tasks from database
-@app.route('/api/get_tasks')
-def get_data():
-    tasks = Todo.query.order_by(Todo.data_created).all()
-    tasks_set = []
-    for task in tasks:
-        task_set = {
-            "id": task.id,
-            "content": task.content,
-            "completed": task.completed,
-            "date": task.data_created
-        }
-        tasks_set.append(task_set)
-    return tasks_set
-
-# APIs
-# API for creating new task and adding its data into a database
-@app.route('api/create_task/', method=['POST'])
-def create_task():
-    content = request.json['content']
-    new_task = Todo(content = content)
-    try:
-        db.session.add(new_task)
-        db.session.commit()
-        return
-    except:
-        return "There was an issue adding your task"
-
-# API for deleting task from database
-@app.route('api/delete_task/<int:id>', method=['DELETE'])
-def delete_task(id):
-    task = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task)
-        db.session.commit()
-        return
-    except:
-        return "There was an issue deleting your task"
-
-# API for updating task
-@app.route('api/update_task/<int:id>', method=['PUT'])
-def update_task(id):
-    task = Todo.query.get_or_404(id)
-    try:
-        task.content = request.json['content']
-        task.completed = request.json['completed']
-        db.session.commit()
-        return
-    except:
-        return "There was an issue updating your task"
 
 if __name__ == '__main__':
     app.run(debug=True)
