1c8c543dd6955d3ca870b83a65efede11dfe5e57
Nnisik <nisik0120@gmail.com>
2024-12-18 23:07:51 +0100

    fixed tasks drawwing

--- client/test.js
+++ client/test.js
@@ -19,134 +19,107 @@ function drawEmptyListMessage() {
     document.querySelector("#tasks-list").appendChild(emptyListMessage);
 }
 
-// Section containing task details (content and creation date)
-function drawTaskInfoSection(content, status, group, priority, date) {
-    const container = document.createElement("div");
-    container.classList.add("task__info");
-
-    // Task content (title)
-    // TODO: modify subclass style base on priority value
-    const header = document.createElement("h3");
-    container.classList.add("task__title");
-    header.innerText = content;
-    container.appendChild(header);
-
-    // TODO: Grouping sections
-    const groupSection = drawTaskGroups(status, group);
-    container.appendChild(groupSection);
-
-    // Task creation date
-    // FIXME: Adjust date format to "day/month"
-    const creationDate = document.createElement("p");
-    creationDate.innerText = date;
-    container.appendChild(creationDate);
-
-    return container;
-}
-
-function drawTaskGroups(status, group) {
-    const groupSection = document.createElement("div");
-    groupSection.classList.add("task__groups");
-
-    // group
-    const groupElement = document.createElement("p");
-    groupElement.classList.add("group");
-    groupElement.innerText = group;
-    groupSection.appendChild(groupElement);
-
-    // TODO: status
-    const statusElement = document.createElement("select");
-    statusElement.classList.add("group", "task-status");
-    // TODO: add options and option select
-    statusElement.innerText = status;
-    groupSection.appendChild(statusElement);
-
-    return groupSection;
-}
-
-// Edit button
-function drawEditButton(id) {
-    const button = document.createElement("button");
-    button.classList.add("task__button", "edit-btn", "no-outline-no-borders");
-    button.setAttribute("data-id", id)
-    const img = document.createElement("img");
-    img.src = "./src/icons/edit-tools.png";
-    button.appendChild(img);
-
-    return button;
-}
-
-// Delete button
-function drawDeleteButton(id) {
-    const button = document.createElement("button");
-    button.classList.add("task__button, delete-btn", " no-outline-no-borders");
-    button.setAttribute("data-id", id)
-    const img = document.createElement("img");
-    img.src = "./src/icons/loschen.png";
-    button.appendChild(img);
-
-    return button;
-
-}
-
-// Section for task action buttons (edit and delete)
-function drawTaskOptionsSection(id) {
-    // Section for task action buttons (edit and delete)
-    const options = document.createElement("div");
-    options.classList.add("task__options");
-
-    // Edit button
-    const editButton = drawEditButton(id);
-
-    // Delete button
-    const deleteButton = drawDeleteButton(id);
-
-    options.append(editButton, deleteButton);
-    return options;
+function capitalizeFirstLetter(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
 
 // Draws the list of tasks in the UI
-function drawTaskList(tasks){
-    /*
-    clearTaskList(); // Ensure the task list is cleared before rendering
-    if (!tasks || tasks.length === 0) {
-        drawEmptyListMessage();
-        return;
-    }
-    */
+function drawTaskList(){
+    let tasks;
+    fetch("http://127.0.0.1:5000/api/tasks/")
+        .then(res => res.json())
+        .then(tasks => {
+            // clearTaskList(); // Ensure the task list is cleared before rendering
+            if (!tasks) {
+                drawEmptyListMessage();
+                return;
+            }
+            console.log(tasks);
 
-    // Iterates over tasks and creates task elements
-    // FIXME: remake for new styling
-    tasks.forEach((task) => {
-        // Container for a single task
-        const taskContainer = document.createElement("div");
-        taskContainer.classList.add("task", "flex");
+            // Iterates over tasks and creates task elements
+            // FIXME: remake for new styling
+            tasks.forEach((task) => {
+                // Container for a single task
+                const taskContainer = document.createElement("div");
+                taskContainer.classList.add("task", "flex");
 
-        // Section containing task details (content and creation date)
-        const taskInfo = drawTaskInfoSection(task["content"]);
-        taskContainer.append(taskInfo);
+                // Task info section
+                const taskInfo = document.createElement("div");
+                taskInfo.classList.add("task__info");
 
-        // Section for task action buttons (edit and delete)
-        const taskOptions = drawTaskOptionsSection();
+                // Task title
+                const taskTitle = document.createElement("h3");
+                taskTitle.innerText = capitalizeFirstLetter(task.content);
+                taskTitle.classList.add("task__title");
+                taskInfo.appendChild(taskTitle);
 
-        taskContainer.append(taskOptions);
-        document.querySelector("#tasks-list").append(taskContainer);
-    });
+                // Task group and status
+                const taskGroups = document.createElement("div");
+                taskGroups.classList.add("task__groups");
+
+                // Group
+                const group = document.createElement("p");
+                group.innerText = task.group;
+                group.classList.add("group");
+                taskGroups.appendChild(group);
+
+                // Status
+                const status = document.createElement("select");
+                status.classList.add("group", "task-status");
+                status.innerHTML = "<option value=0>Not started</option><option value=1>In progress</option><option value=2>Done</option>";
+                taskGroups.appendChild(status);
+                taskInfo.appendChild(taskGroups);
+
+                // Task date
+                const taskDate = document.createElement("p");
+                taskDate.innerText = task.date.year + "/" + task.date.month + "/" + task.date.day;
+                taskDate.classList.add("task__date");
+                taskInfo.appendChild(taskDate);
+                taskContainer.appendChild(taskInfo);
+
+                // Task options
+                const taskOptionsContainer = document.createElement("div");
+                taskOptionsContainer.classList.add("task__options");
+
+                // Edit button
+                const editButton = document.createElement("button");
+                editButton.classList.add("task__button", "edit-btn", "no-outline-no-border");
+                editButton.setAttribute("data-id", task.id);
+                editButton.setAttribute("aria-label", "Edit task");
+                const editIcon = document.createElement("img");
+                editIcon.classList.add("task__icon");
+                editIcon.setAttribute("src", "src/icons/edit-tools.png");
+                editIcon.setAttribute("alt", "edit");
+                editButton.appendChild(editIcon);
+                taskOptionsContainer.appendChild(editButton);
+                // Delete button
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("task__button", "delete-btn", "no-outline-no-border");
+                deleteButton.setAttribute("data-id", task.id);
+                deleteButton.setAttribute("aria-label", "Delete task");
+                const deleteIcon = document.createElement("img");
+                deleteIcon.classList.add("task__icon");
+                deleteIcon.setAttribute("src", "src/icons/loschen.png");
+                deleteIcon.setAttribute("alt", "delete");
+                deleteButton.appendChild(deleteIcon);
+                taskOptionsContainer.appendChild(deleteButton);
+
+                taskContainer.appendChild(taskOptionsContainer);
+                document.querySelector("#tasks-list").append(taskContainer);
+            });
+        })
+        .catch(err => console.log(err));
 }
 
 // Fetches tasks from the API and returns them
-async function getTasks() {
-    try {
-        const response = await fetch("http://127.0.0.1:5000/api/tasks/");
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-        tasks = await response.json();
-        return tasks;
-    } catch (error) {
-        // Logs errors during the fetch or processing
-        console.error("Error fetching posts:", error);
-    }
+function getTasks() {
+    // FIXME: test if it even goes for date
+    fetch("http://127.0.0.1:5000/api/tasks/")
+        .then(res => res.json())
+        .then(data => {
+            return data;
+        })
+        .catch(err => console.log(err));
 }
 
 // Creates a new task and sends it to the server via a POST request
@@ -155,12 +128,14 @@ function createNewTask() {
 
     // Getting and formatting the current date and time as "YYYY-MM-DD HH:MM:SS"
     const now = new Date();
+    let formatedDate = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
 
     // Sending a POST request to the server with the new task's content and creation date
     fetch("http://127.0.0.1:5000/api/tasks/", {
         method: "POST",
         body: JSON.stringify({
-            content: taskContent
+            content: taskContent,
+            date: formatedDate
         })
     })
         .then((response) => response.json() // Parses the JSON response
@@ -213,9 +188,8 @@ function sortTasksByDate(tasks) {
 }
 
 // Initializes the application once the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", async () => {
-    await getTasks(); // Fetches tasks
-    drawTaskList(tasks); // Displays tasks on page load
+document.addEventListener("DOMContentLoaded", () => {
+    drawTaskList(); // Displays tasks on page load
 
     // Opens the "Add Task" modal
     document.getElementById("add-task-btn").addEventListener("click", () => {
